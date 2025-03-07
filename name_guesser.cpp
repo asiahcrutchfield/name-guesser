d1fe7936651fb97c12414dd3e7c3d51898eff51d
Asiah Crutchfield <195882349+asiahcrutchfield@users.noreply.github.com>
2025-03-07 20:34:52 +0800

    downloaded json for c++ and included it in my c++ file

--- name_guesser.cpp
+++ name_guesser.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "functions.hpp"
+#include "functions.hpp"
+#include <nlohmann/json.hpp>
 using namespace std;
 
 int main() {
-
+    cout << "Hello, Welcome to the \"Name Game\"! You give me your name and I try to determine if you're lying.\n";
+    cout << "If I win, I get to gloat about how superior I am. If you win... I guess I'll give you validation. You humans like that right?\n";
+    cout << "Anyway, let's stop wasting time and see if I can catch you in a lie!";
 }
