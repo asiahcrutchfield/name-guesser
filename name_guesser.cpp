231e7b29bea7fbd6be7a6ae0d67dfffc70b9262f
Asiah Crutchfield <195882349+asiahcrutchfield@users.noreply.github.com>
2025-03-07 20:36:58 +0800

    updated name_guesser

--- name_guesser.cpp
+++ name_guesser.cpp
@@ -7,5 +7,6 @@ using namespace std;
 int main() {
     cout << "Hello, Welcome to the \"Name Game\"! You give me your name and I try to determine if you're lying.\n";
     cout << "If I win, I get to gloat about how superior I am. If you win... I guess I'll give you validation. You humans like that right?\n";
-    cout << "Anyway, let's stop wasting time and see if I can catch you in a lie!";
+    cout << "Anyway, let's stop wasting time and see if I can catch you in a lie!\n";
+    cout << "What is your name? ";
 }
